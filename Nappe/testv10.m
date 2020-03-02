3300e424637f29fdb99ac74b72cdf7357c881d48
cgouiller <clem.gouiller@gmail.com>
2020-03-02 10:59:23 +0100

    ici

--- Nappe/testv10.m
+++ Nappe/testv10.m
@@ -1,4 +1,3 @@
-%test
 [im, imref, param]=mCINREAD2(strcat('D:\2019_12_gouiller\',dateCatalogue{numVid},'\',num2str(numProf{numVid}),'_',num2str(numPassage(numVid)),'.mcin2'),1,nbImg(numVid));
 
 %%
